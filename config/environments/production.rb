198c7c5b5ed692838433d54053c171b33f81d350
Jeff Chen <hi@jeffchen.ca>
2013-10-30 00:07:21 -0700

    Added Devise Gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Require for Heroku
+  #Note to set this to your actual host
+  config.action_mailer.default_url_options = { :host => 'http://jeffchensapp.herokuapp.com' }
 end
